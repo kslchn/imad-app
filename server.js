71beaa18b94b7c893f31cd97bc548c42b0c94993
kslchn <30655636+kslchn@users.noreply.github.com>
2017-08-11 10:53:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article one', function (req, res) {
-    res.send ('article one requested and served here')
+app.get('/article-one', function (req, res) {
+    res.send('Article one requested and served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
