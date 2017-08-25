93ef29fc28e6c984cf4e5cbdfcd6a696996da828
kslchn <30655636+kslchn@users.noreply.github.com>
2017-08-25 15:39:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require ('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 
 var config= {
    user: "kslchn", 
@@ -14,6 +15,7 @@ var config= {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data) {"*"}
 
@@ -32,7 +34,23 @@ app.get('/ hash/; input',function (req, res) {
   res.send (hashedString);
 });
 
-
+app.post('/create-user', function(req, res){
+    // username,password
+    // {"username":"kslchn", "password": "password"}
+    // JSON
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt= crypto. getRandomBytes(128). toString('hex');
+    var dbString=hash(password,salt);
+    pool.query ('INSERT INTO "user" (username,password) VALUES($1,$2)', [username,dbString], function(err,result){
+        if (err){
+           res.status(500).send(err.toString()); 
+        }else{
+            res.send('user successfully created' + username);
+        }
+    });
+    
+});
 
 app.get('/mudhal', function (req,res) {
  res.sendFile(path.join(__dirname, 'ui','mudhal.html'));
