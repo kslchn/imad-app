3781c48af4b90d8aee652f3696663d8c732349d1
kslchn <30655636+kslchn@users.noreply.github.com>
2017-08-25 14:03:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require ('pg').Pool;
+var crypto=require('crypto');
+
+var config= {
+   user: "kslchn", 
+   database: "kslchn",
+   host: "db.hasura-app.io",
+   port: "5432",
+   password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate (data) {'*'}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash (input,salt) {
+    
+    var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512'); 
+    return hashed. toString ('hex');
+}
+
+app.get('/ hash/; input',function (req, res) {
+  var hashedString=hash(req.params.input,'this-is-some-random-string');
+  res.send (hashedString);
+});
+
+
 
 app.get('/mudhal', function (req,res) {
  res.sendFile(path.join(__dirname, 'ui','mudhal.html'));
@@ -15,16 +40,20 @@ app.get('/mudhal', function (req,res) {
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+
+  
+
 app.get('/second', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'second.html'));
 });
 
 app.get('/articletwo',function (req,res){
    res.send('article two requested and will be served here');
-})
+});
+
 app.get('/articlethree',function (req,res){
    res.send('article one requested and will be served three');
-})
+});
 
 
 app.get('/ui/style.css', function (req, res) {
