87e798cd6e51edaef98b78fb9d16f98f7f60568f
kslchn <30655636+kslchn@users.noreply.github.com>
2017-08-26 11:26:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,6 +52,20 @@ app.post('/create-user', function(req, res){
     
 });
 
+app.post('/login', function(req, res) {
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt= crypto. getrandomBytes(128). toString('hex');
+    var dbString=hash(password,salt);
+    pool.query ('INSERT INTO "user" (username,password) VALUES($1,$2)', [username,dbString], function(err,result){
+        if (err){
+           res.status(500).send(err.toString()); 
+        }else{
+            res.send('user successfully created' + username);
+        }
+    }); 
+});
+
 app.get('/mudhal', function (req,res) {
  res.sendFile(path.join(__dirname, 'ui','mudhal.html'));
 });
